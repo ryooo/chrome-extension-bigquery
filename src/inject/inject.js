07247581fd476ea7dbaf2b0d6d44531e742b7129
ryooo <ryooo.321@gmail.com>
2021-09-07 17:54:03 +0900

    複数タブ対応、日本語対応、10TB以上はモーダル

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,60 +1,78 @@
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 		if (document.readyState === "complete") {
-			clearInterval(readyStateCheckInterval);
-
+			// clearInterval(readyStateCheckInterval);
 			attach();
 		}
-	}, 10);
+	}, 1000);
 });
+function collectionHas(a, b) { //helper function (see below)
+    for(var i = 0, len = a.length; i < len; i ++) {
+        if(a[i] == b) return true;
+    }
+    return false;
+}
+function findParentBySelector(elm, selector) {
+    var all = document.querySelectorAll(selector);
+    var cur = elm.parentNode;
+    while(cur && !collectionHas(all, cur)) { //keep going up until you find a match
+        cur = cur.parentNode; //go up
+    }
+    return cur; //will return null if not found
+}
 
 const attach = function() {
-	console.log("Attaching to run button and query validation status...")
-	const runButton = document.querySelector(".bqui-test-run-query");
-	const queryValidationStatus = document.querySelector("query-validation-status");
-	console.log(queryValidationStatus);
-	const callback = function(changes) {
-		const text = queryValidationStatus.textContent
-		console.log(text);
-		const regex = /This query will process (?<number>\d\S*) (?<unit>\S+)/;
-		const match = regex.exec(text);
+	const queryValidationStatuses = document.querySelectorAll(".query-validation-status:not(.query-check-extension-attached)");
+	queryValidationStatuses.forEach(function(queryValidationStatus, i) {
+		queryValidationStatus.classList.add("query-check-extension-attached")
+		const runButton = findParentBySelector(queryValidationStatus, ".cfc-action-bar-layout-parent").querySelector(".bqui-test-run-query");
+		console.log(queryValidationStatus);
+		const callback = function(changes) {
+			const text = queryValidationStatus.textContent
+			console.log(text);
+			const regex = /このクエリを実行すると、(?<number>\d\S*) (?<unit>\S+)/;
+			const match = regex.exec(text);
 
-		if (!match) {
-			return;
-		}
-
-		console.log(match.groups);
-		const unitToExponent = ["B", "KiB", "MiB", "GiB", "TiB"];
-		const numberOfBytes =  match.groups.number * Math.pow(1024, unitToExponent.indexOf(match.groups.unit))
-		console.log("# of bytes: " + numberOfBytes);
-		const estimatedCost = Math.round(5.00 * numberOfBytes / Math.pow(1024, 4)*100)/100;
-		console.log("Estimated cost: " + estimatedCost + " $ USD");
-
-		runButton.textContent = "Run @ " + estimatedCost + " $ USD";
-		runButton.disabled = false;
-		if (estimatedCost >= 5) {
-			runButton.style.backgroundColor = "#da7373";
-			runButton.disabled = true;
-		} else if (estimatedCost >= 2) {
-			runButton.style.backgroundColor = "#e6ea02";
-		} else {
-			runButton.style.backgroundColor = "#3367d6";
-		}
-	};
-
-	const mutationObserver = new MutationObserver(callback);
-	mutationObserver.observe(queryValidationStatus, {childList: true, subtree: true});
-
-	runButton.onclick = function() {
-		mutationObserver.disconnect();
-		const checkReadyToAttachInterval = setInterval(function() {
-			console.log("Waiting for query to complete to attach to DOM...");
-			const queryValidationStatus = document.querySelector("query-validation-status");
-			if (!queryValidationStatus) {
+			if (!match) {
 				return;
 			}
-			clearInterval(checkReadyToAttachInterval)
-			attach();
-		}, 1000);
-	}
+
+			const unitToExponent = ["B", "KiB", "MiB", "GiB", "TiB"];
+			const numberOfBytes =  match.groups.number * Math.pow(1024, unitToExponent.indexOf(match.groups.unit))
+			const estimatedCost = Math.round(6.00 * numberOfBytes / Math.pow(1024, 4)*100)/100;
+
+			runButton.textContent = "実行 " + match.groups.number + match.groups.unit + " @ " + estimatedCost + " $ USD";
+			runButton.disabled = false;
+			if (match.groups.unit == "TiB" && match.groups.number >= 10) {
+				runButton.style.backgroundColor = "#ff7f7f";
+				runButton.disabled = true;
+				setTimeout(function() {
+					alert("このSQLは検索範囲が大きいため、実行できません。\n条件式に分割フィールドを加えて検索範囲を狭めてください。\n" + match.groups.number + match.groups.unit + " \n@ " + estimatedCost + " $ USD")
+				}, 10)
+			} else if (estimatedCost >= 5) {
+				runButton.style.backgroundColor = "#da7373";
+				runButton.disabled = false;
+			} else if (estimatedCost >= 2) {
+				runButton.style.backgroundColor = "#e6ea02";
+			} else {
+				runButton.style.backgroundColor = "#3367d6";
+			}
+		};
+
+		const mutationObserver = new MutationObserver(callback);
+		mutationObserver.observe(queryValidationStatus, {childList: true, subtree: true});
+
+		runButton.onclick = function() {
+			mutationObserver.disconnect();
+			const checkReadyToAttachInterval = setInterval(function() {
+				console.log("Waiting for query to complete to attach to DOM...");
+				const queryValidationStatus = document.querySelector("query-validation-status");
+				if (!queryValidationStatus) {
+					return;
+				}
+				clearInterval(checkReadyToAttachInterval)
+				attach();
+			}, 1000);
+		}
+	});
 };
